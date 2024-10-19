5af2334d6013f12357837ac7ce0ecc9a068e0daf
[200~Dhanusha0606~ <divya1ballikhana@gmail.com>
2024-10-18 21:40:20 -0400

    sdet salesforce selenium user stories

---
+++ src/main/java/SDETSelenium/CheapOAir.java
@@ -0,0 +1,71 @@
+package SDETSelenium;
+
+
+/*
+ * https://www.cheapoair.com/
+ *1. Select one way trip
+ *2. Select Destination
+ *3. Select Travel date
+ *4. Select 2 adults & Click on Done
+ *5. Click search Flights
+ *6. Write a logic to find the lowest price and Select the lowest price flight
+ * 
+ */
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+
+public class CheapOAir {
+
+	public static void main(String[] args) throws InterruptedException {
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.get("https://www.cheapoair.com/");
+       
+        WebElement eletrip=driver.findElement(By.id("onewayTrip"));
+        driver.executeScript("arguments[0].click()", eletrip);
+		//driver.findElement(By.id("onewayTrip")).click();
+
+		WebElement elefrom=driver.findElement(By.id("from0"));
+		elefrom.sendKeys(Keys.CONTROL,"a");
+		elefrom.sendKeys(Keys.DELETE);
+		elefrom.sendKeys("MAA",Keys.TAB);
+		WebElement textBox=driver.findElement(By.id("to0"));
+		textBox.sendKeys(Keys.CONTROL,"a");
+		textBox.sendKeys(Keys.DELETE);
+		textBox.sendKeys("BLR",Keys.ENTER);
+		driver.findElement(By.xpath("//a[@class=' month-date is--today']")).click();
+		driver.findElement(By.xpath("//div[text()='Traveler']")).click();
+		driver.findElement(By.id("addadults")).click();
+		driver.findElement(By.id("closeDialog")).click();
+		driver.findElement(By.id("searchNow")).click();
+
+		List<WebElement> allElements = driver.findElements(By.xpath("//span[@class='currency text-nowrap']"));
+		List<Double> allPrice = new ArrayList<Double>();
+		for (WebElement eachEle : allElements) {
+			String priceInString = eachEle.getAttribute("title");
+			double priceInDouble = Double.parseDouble(priceInString);
+			allPrice.add(priceInDouble);
+		}
+
+		Collections.sort(allPrice); // Write a logic to find the least price
+		Double least = allPrice.get(0);
+
+		driver.findElement(By.xpath("//span[contains(@title,'" + least + "')]/following::span[text()='Select']"))
+				.click();
+
+	}
+
+}
+
